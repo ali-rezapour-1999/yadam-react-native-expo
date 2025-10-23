131fca69e87265e3d119e05252fad52db50ea747
ali.programer.1999 <rezapour.gatabi.1999@gmail.com>
2025-10-23 11:27:37 +0330

    fix: change call in datepicker

--- components/common/datePicker.tsx
+++ components/common/datePicker.tsx
@@ -1,23 +1,13 @@
-import DateTimePicker from '@react-native-community/datetimepicker';
 import { ControllerRenderProps, Path } from 'react-hook-form';
-import { useAppStore } from '@/store/appState';
 import YearCalendar from '../shared/forms/yearCalender';
 
 interface DatePickerProps<T extends Record<string, any>> {
   field: ControllerRenderProps<T, Path<T>>;
-  setShowDatePicker: React.Dispatch<React.SetStateAction<boolean>>;
-  showDatePicker: boolean;
 }
 
-const DatePicker = <T extends Record<string, any>>({ field, showDatePicker }: DatePickerProps<T>) => {
+const DatePicker = <T extends Record<string, any>>({ field }: DatePickerProps<T>) => {
 
-  return (
-    <>
-      {showDatePicker && (
-        <YearCalendar selectedDate={field.value} setSelectedDate={field.onChange} />
-      )}
-    </>
-  );
+  return <YearCalendar selectedDate={field.value} setSelectedDate={field.onChange} />
 };
 
 export default DatePicker;
